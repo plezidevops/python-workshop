f81a9af6306944431dbed9b377be619a767f9e7f
Plezi Devops <plezidevops@gmail.com>
2023-07-02 18:50:28 -0400

    assigning values to variables

---
+++ activities/activity1-assigning-values-to-variables.py
@@ -0,0 +1,11 @@
+# assign a number to the x variable, increment the number, and perform additional operations.
+
+import math
+import keyword
+
+x = 14
+x += 1
+print((x / 5)**2)  # 9
+
+
+print(keyword.kwlist)
