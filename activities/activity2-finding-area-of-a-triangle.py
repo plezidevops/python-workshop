43b74ab06bccd53c5333e6f3d2e7ddbe1afed635
Plezi Devops <plezidevops@gmail.com>
2023-07-03 22:48:30 -0400

    chapter 1 activities

---
+++ activities/activity2-finding-area-of-a-triangle.py
@@ -0,0 +1,10 @@
+"""
+This program determine the area of a triangle using variables, comments, type conversion, and docstrings.
+
+"""
+
+base, height = 2, 4
+
+area = int(base * height / 2)
+
+print(area)
