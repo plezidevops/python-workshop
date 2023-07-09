706979241acc51dd0b715641c8c6bdfb92f14f52
Plezi Devops <plezidevops@gmail.com>
2023-07-09 12:22:48 -0400

    creating and import  module

---
+++ excercises/chp3/exercise36.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This script computes the sum of the factorial of a
+list of numbers"""
+
+from  exercise36_first_module import factorial_sum
+
+print(factorial_sum([5, 7,11]))
---
+++ excercises/chp3/exercise37.py
@@ -0,0 +1,4 @@
+import exercise36_first_module as my_module
+
+help(my_module)
+print(my_module.__doc__)
