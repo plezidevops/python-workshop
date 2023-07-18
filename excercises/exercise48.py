fc4ff543a2f12b4f8cc66e37c4a1a0ebb735ed24
Plezi Devops <plezidevops@gmail.com>
2023-07-17 21:47:56 -0400

    convert and sum list kwargs

--- excercises/exercise48.py
+++ excercises/exercise48.py
@@ -1,10 +1,17 @@
-def convert_ust_to_aud(amount, rate=0.75) :
+def convert_usd_to_aud(amount, rate=0.75) :
     return amount / rate
 
 def convert_and_sum_list(usd_list, rate=0.75):
     total = 0
     for amount in usd_list:
-        total += convert_ust_to_aud(amount, rate=rate)
+        total += convert_usd_to_aud(amount, rate=rate)
+    return total
+
+def convert_and_sum_list_kwargs(usd_list, **kwargs):
+    total = 0
+    for amount in usd_list:
+        total += convert_usd_to_aud(amount, **kwargs)
     return total
 
 print(convert_and_sum_list([1, 3]))
+print(convert_and_sum_list_kwargs([1, 4], rate=0.8))
